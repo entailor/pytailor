a8396b7c55e1495dec1dee57ab0fe519712936a2
Audun Gravdal Johansen <audun@entail.no>
2020-08-07 11:02:16 +0200

    Use httpx status code constants

--- tailor/services/user_service.py
+++ tailor/services/user_service.py
@@ -1,11 +1,12 @@
 from .client import client
 from ..models import UserModel
 from ..exceptions import ApiResponseError
+import httpx
 
 
 def get_user() -> UserModel:
     resp = client.get('user/profile')
-    if resp.status_code == 200:
+    if resp.status_code == httpx.codes.OK:
         return UserModel.from_dict(resp.json())
     else:
         raise ApiResponseError(f'Unexpected API response: {str(resp)}')
