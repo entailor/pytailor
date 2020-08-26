eafb1b22138fe8909630fde9fda3ee3176404719
Audun Gravdal Johansen <audun@entail.no>
2020-08-26 14:07:58 +0200

    update user client

--- tailor/clients/user_client.py
+++ tailor/clients/user_client.py
@@ -1,22 +1,19 @@
-from httpx import Client
 from pydantic import ValidationError
-
-from tailor.models import User, UserSummary
-from tailor.config import API_BASE_URL, AUTH_KEY
-from .auth import TailorAuth
-# from urllib.parse import urljoin
+from typing import List
+from tailor.models import User
+from .base_client import BaseClient
 
 
-class UserClient(Client):
-    def __init__(self):
-        super().__init__(base_url=API_BASE_URL, auth=TailorAuth(AUTH_KEY))
+class UserClient(BaseClient):
 
-    def get_current_user(self):
-        request = self.get('profile')
-
-        try:
-            user = User.parse_obj(request.json())
-        except ValidationError:
-            print('ValidationError...')
-
+    def get_current_user(self) -> User:
+        url = 'profile'
+        request = self.get(url)
+        user = User.parse_obj(request.json())
         return user
+
+    def get_project_users(self, project_uuid: str) -> List[User]:
+        url = f'projects/{project_uuid}/users'
+        request = self.get(url)
+        project_users = [User.parse_obj(obj) for obj in request.json()]
+        return project_users
