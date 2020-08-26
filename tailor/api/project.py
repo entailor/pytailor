e2f2b7365a65c560df6537b170a6dc04f95c57d1
Audun Gravdal Johansen <audun@entail.no>
2020-08-26 16:29:56 +0200

    update Project user class

--- tailor/api/project.py
+++ tailor/api/project.py
@@ -18,3 +18,15 @@ class Project:
         with ProjectClient() as client:
             self.__project_model: ProjectModel = client.get_project(self.uuid)
         self.name = self.__project_model.name
+
+    def __repr__(self):
+        return f'Project(uuid={self.uuid})'
+
+    @classmethod
+    def from_name(cls, project_name: str) -> 'Project':
+        """Get project with name *project_name*."""
+        with ProjectClient() as client:
+            for prj in client.get_projects():
+                if project_name == prj.name:
+                    return Project(prj.uuid)
+            raise ValueError(f'Could not find project with name {project_name}')
