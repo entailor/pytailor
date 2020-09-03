deb9703a2163c68a0498a08d36518c1e55b487a5
Audun Gravdal Johansen <audun@entail.no>
2020-09-03 10:12:49 +0200

    add kwargs example

---
+++ examples/example02_kwargs.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Backend Example 2
+
+This example introduces the following NEW concepts:
+    - For PythonTasks:
+        - Specifying keyword arguments (**kwargs) to the function
+"""
+
+from tailor import PythonTask, DAG, Workflow, Project
+
+### workflow definition ###
+
+t1 = PythonTask(
+    function='time.sleep',
+    name='task 1',
+    args=1
+)
+t2 = PythonTask(
+    function='builtins.print',
+    name='task 2',
+    args=['\nSlept for', '1', 'second'],
+    kwargs={'sep': '   ', 'end': '\n\n'},
+    parents=t1
+)
+
+dag = DAG(tasks=[t1, t2], name='dag')
+
+### workflow run ###
+
+# Open a project.
+project_uuid = "702d688e-972d-4580-afa2-fc616533ccba"
+prj = Project(project_uuid)
+
+# create a workflow:
+wf = Workflow(project=prj, dag=dag, name='kwarg workflow')
+
+# run the workflow
+wf.run()
+
+# check the status of the workflow run
+print(wf.state)
