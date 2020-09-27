9525b8edb10ee781372d688a43bf94b1e22e17f2
Audun Gravdal Johansen <audun@entail.no>
2020-09-27 13:17:58 +0200

    add script to run all test scripts with PyCharms runfile function

---
+++ test_scripts/run_all_test_scripts.py
@@ -0,0 +1,5 @@
+# runfile is a special function avalable in the pycharm python console
+runfile('test_2level_branching.py', wdir='.')
+runfile('test_multiple_files.py', wdir='.')
+runfile('test_nested_branching_data.py', wdir='.')
+runfile('test_scope_inheritance.py', wdir='.')
