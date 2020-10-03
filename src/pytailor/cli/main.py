c3bf772fcb80fc63a114adee456e3341a2c63039
agrav <audun.gravdal.johansen@gmail.com>
2020-10-03 14:13:02 +0200

    Feature/test worker (#6)
    * added testworker that tries to import all functions in workflow-definitions in the project(s) tehe worker has access to
    
    * minor stuff
    
    * include checks under the tailor worker command with associated flags.
    
    Dont use user-API classes in internal code, use Rest models directly.
    
    * update logging from worker checks
    
    Co-authored-by: bernt sørby <bsorby@gmail.com>
    Co-authored-by: Audun Gravdal Johansen <audun@entail.no>

--- src/pytailor/cli/main.py
+++ src/pytailor/cli/main.py
@@ -5,6 +5,7 @@ import toml
 
 from pytailor.utils import default_worker_name
 from pytailor.execution.worker import run_worker
+from pytailor.execution.worker_checks import workflow_definition_compliance_test
 
 
 @click.group()
@@ -28,8 +29,23 @@ def cli():
     help="Add a project filter",
     multiple=True,
 )
-def worker(sleep, ncores, workername, project_id_filter):
+@click.option(
+    "--checks/--no-checks",
+    default=True,
+    help="Perform checks to validate the worker environment before starting the worker"
+)
+@click.option(
+    "--checks-only", is_flag=True, help="Perform checks without starting the worker"
+)
+def worker(sleep, ncores, workername, project_id_filter, checks, checks_only):
     """Start a worker process."""
+    if checks:
+        # check compliance with project wf defs
+        wf_defs_info = workflow_definition_compliance_test(project_id_filter)
+        # TODO: create a wf_def_filter based on wf_defs_info ?
+
+    if checks_only:
+        return
     run_worker(sleep, ncores, workername, project_id_filter)
 
 
--- src/pytailor/execution/worker.py
+++ src/pytailor/execution/worker.py
@@ -18,8 +18,7 @@ async def do_checkout(checkout_query: TaskCheckout) -> Optional[TaskExecutionDat
             exec_data = await client.checkout_task(checkout_query)
         except httpx.HTTPError as exc:
             raise BackendResponseError(
-                f"Error while checking out task. The response "
-                f"was: {str(exc)}."
+                f"Error while checking out task. The response " f"was: {str(exc)}."
             )
     return exec_data
 
