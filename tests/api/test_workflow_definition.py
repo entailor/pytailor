70987cea0b4b426c2f9fc52bcc24f19c93ad01f0
Mads Fredrik Heiervang <mheeirvang@gmail.com>
2020-11-03 14:50:44 +0100

    added assertion for test workflow definition

--- tests/api/test_workflow_definition.py
+++ tests/api/test_workflow_definition.py
@@ -1,11 +1,10 @@
 from unittest.mock import patch, Mock
 import pytest
-
+from tests.api.schema.testdata.input_data import inputs1
 from pytailor import WorkflowDefinition, DAG, Account, Workflow
 from pytailor.models import WorkflowDefinition as WorkflowDefinitionModel
-from pytailor.models import WorkflowDefinitionCreate
-from pytailor.models import Account as AccountModel
-from pytailor.exceptions import BackendResponseError
+
+from .schema.test_inputs import schema_validator
 from ..data import dag_data, data_workflow
 
 
@@ -16,8 +15,8 @@ def dag():
 
 def test_create_wf_def():
     wf_def = WorkflowDefinition(
-        name=Mock(),
-        description=Mock(),
+        name='asdf',
+        description='asdf',
         dag=dag
     )
     assert wf_def.dag == dag
@@ -41,20 +40,19 @@ def test_add_to_account(mocked_method):
         dag=DAG.from_dict(dag_data)
     )
     wf_def.add_to_account(Account(Mock()))
-    assert wf_def.id == wf_def_model.id
+    assert wf_def.id == 'asdf'
     mocked_method.assert_called_once()
 
 
-# @patch("pytailor.api.dag.DAG.to_dict",
-#        return_value=dag_data)
 def test_from_workflow():
     mock_wf = Mock(spec=Workflow)
-    mock_wf.dag.to_dict.return_value = dag_data
-    mock_wf.inputs = {}
+    mock_wf.dag = DAG.from_dict(dag_data)
+    mock_wf.inputs = inputs1
     wf_def = WorkflowDefinition.from_workflow(
         wf=mock_wf,
         wf_def_name='asdf',
         wf_def_description='asdf',
     )
-
-
+    wf_def_dagdict = wf_def.dag.to_dict()
+    assert wf_def_dagdict['name'] == dag_data['name']
+    assert not schema_validator(wf_def.inputs_schema)
